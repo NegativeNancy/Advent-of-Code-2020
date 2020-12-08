0eee8d8379bb00cca3b6cf1a942b616905260e17
Ivo den Hertog <ivodenhertog@gmail.com>
2020-12-09 00:54:09 +0100

    Day 6 - solution 2 finished

--- day6.py
+++ day6.py
@@ -34,8 +34,48 @@ def solution1(answers):
     print("Total of submited answers:", answers)
 
 
+def duplicate_answers(data):
+    duplicate = 0
+    group_size = len(data.splitlines())
+
+    for char in set(data) :
+        if (char.isalpha() == True):
+            counts = data.count(char)
+            if (counts == group_size):
+                duplicate += 1
+    return duplicate
+
+
+def get_group(data):
+    counter = 0
+    group_data = {}
+    temp_data = ''
+
+    for line in data:
+        if (len(data) == counter + 1):
+            temp_data += line + "\n"
+            group_data[counter] = temp_data
+        elif (line.isalpha() == True):
+            temp_data += line + "\n"
+            counter += 1
+        else:
+            group_data[counter] = temp_data
+            temp_data = ''
+            counter += 1
+    
+    return group_data
+
+
+def solution2(group_data):
+    total = 0
+    for value in group_data.values():
+        total += duplicate_answers(value)
+    print("Sum of questions where all group members answered with yes:", total)
+
+
 if __name__ == "__main__":
     # data = get_input("test.txt")
     data = get_input("day6.txt")
-    answers = unique_answers(data)
-    solution1(answers)
+    solution1(unique_answers(data))
+    solution2(get_group(data))
+    
