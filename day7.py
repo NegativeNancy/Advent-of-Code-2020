c2b2750f923cbd53632491c0a011e81c2495c4ec
Ivo H <23632852+NegativeNancy@users.noreply.github.com>
2020-12-12 17:51:36 +0100

    Day 7 - solution 2

--- day7.py
+++ day7.py
@@ -8,7 +8,7 @@ def get_input(location):
             for color in contains.split(','):
                 color = color.strip()
                 color = color.strip("bags.")
-                colors.append(color)
+                colors.append(color.strip())
             bags[bag] = colors
        
     return bags
@@ -25,16 +25,32 @@ def can_contain(bags, color, contains):
     return contains
 
 
+def bags_required(bags, input_color):
+    amount = 0 
+    for bag in bags:
+        bag_content = bags.get(bag)
+        if bag == input_color:
+            for item in bag_content:
+                value = item[:2]
+                if value != "no":
+                    if bags_required(bags, item[2:]) == 0:
+                        amount += int(value) * 1
+                    else:
+                        amount += int(value) + (int(value) * bags_required(bags, item[2:]))
+
+    return amount
+
+
 def solution1(bags):
     print("Number of bags that can contain \'shiny gold\' bags:", len(can_contain(bags, "shiny gold", [])))
     
 
 def solution2(bags):
-    pass
+    print("Number of bags inside \"shiny gold\":", bags_required(bags, "shiny gold"))
 
 
 if __name__ == "__main__":
     # bags = get_input("test.txt")
     bags = get_input("day7.txt")
     solution1(bags)
-    # solution2()
+    solution2(bags)
